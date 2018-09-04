b3f645e1d76677df21128cfeebbd1c8c222a5ab5
Phillip Nunes <phillip_nunes@hotmail.com>
2018-09-04 15:28:09 -0300

    Some style in header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Header from './components/Header'
-import Container from './components/Container'
+import { Container } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <Container content={'Text here!!!!!!!!!'} />
-      </div>
+        <Container>
+          <Header />
+        </Container>
     );
   }
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
+import { Grid } from 'semantic-ui-react'
+
+const styles = {
+  content: {
+    fontFamily: 'Open Sans, sans-serif',
+    fontSize: '14px',
+    color: '#666'
+  },
+  colors: {
+    blueBold: {
+      color: '#5d73e5',
+      fontWeight: '600'
+    },
+    green: { color: '#3c763d' },
+    red: { color:  '#a94442'}
+  }
+}
 
 export default class Header extends Component {
   render() {
     return (
-      <div>Just a header</div>
+      <Grid>
+        <Grid.Column mobile={16} computer={4}>
+          <div style={styles.content}>
+            <span>Market Cap: </span>
+            <span style={styles.colors.blueBold}>$239,765,933,840</span>
+          </div>
+        </Grid.Column>
+        <Grid.Column mobile={16} computer={4}>
+          <div style={styles.content} className="header__content">
+            <span style={styles.colors.green}>1.74%</span>
+            <span> in </span>
+            <span style={styles.colors.blueBold}>24 hours</span>
+          </div>
+        </Grid.Column>
+        <Grid.Column mobile={16} computer={4}>
+          <div style={styles.content} className="header__content">
+            <span style={styles.colors.red}>-1.74%</span>
+            <span> in </span>
+            <span style={styles.colors.blueBold}>24 hours</span>
+          </div>
+        </Grid.Column>
+      </Grid>
     )
   }
 }
