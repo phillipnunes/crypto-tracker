d08725f1a30e3c029ce32e447db9e40fbd0ebda0
Phillip Nunes <phillip_nunes@hotmail.com>
2018-09-06 20:05:04 -0300

    Consuming API and showing in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,27 +19,35 @@ const styles = {
 }
 
 export default class Header extends Component {
+  constructor() {
+    super()
+    this.state = {
+      market_cap: 0,
+      last_day: 0,
+      last_hour: 0
+    }
+  }
   render() {
     return (
       <Grid>
         <Grid.Column mobile={16} computer={4}>
           <div style={styles.content}>
             <span>Market Cap: </span>
-            <span style={styles.colors.blueBold}>$239,765,933,840</span>
+            <span style={styles.colors.blueBold}>${this.state.market_cap}</span>
           </div>
         </Grid.Column>
         <Grid.Column mobile={16} computer={4}>
           <div style={styles.content} className="header__content">
-            <span style={styles.colors.green}>1.74%</span>
+            <span style={styles.colors.green}>{this.state.last_day}%</span>
             <span> in </span>
             <span style={styles.colors.blueBold}>24 hours</span>
           </div>
         </Grid.Column>
         <Grid.Column mobile={16} computer={4}>
           <div style={styles.content} className="header__content">
-            <span style={styles.colors.red}>-1.74%</span>
+            <span style={styles.colors.red}>{this.state.last_hour}</span>
             <span> in </span>
-            <span style={styles.colors.blueBold}>24 hours</span>
+            <span style={styles.colors.blueBold}>past hour</span>
           </div>
         </Grid.Column>
       </Grid>
@@ -49,16 +57,33 @@ export default class Header extends Component {
     this.getData();
   }
   getData() {
-    const config = {
-      headers: {'Content-Type': 'application/json'}
-    }
-    axios.get('https://chasing-coins.com/api/v1/std/marketcap/', config)
-      .then(function (response) {
-        console.log(response)
+    axios.get('https://chasing-coins.com/api/v1/std/marketcap/')
+      .then(response => {
+        let filteredValues = this.filterValues(response.data.change)
+
+        this.setState({
+          market_cap: response.data.market_cap,
+          last_day: filteredValues.last_day,
+          last_hour: filteredValues.last_hour
+        })
       })
       .catch(function (error) {
-        // handle error
         console.log(error);
       })
+  }
+  filterValues(values) {
+    let last_day = 0
+    let last_hour = 0
+
+    Object.entries(values).forEach(([key, value]) => {
+      if (key === '24h') {
+        last_day = value
+      }
+      if (key === '1h') {
+        last_hour = value
+      }
+    })
+
+    return { last_day, last_hour }
   }
 }
